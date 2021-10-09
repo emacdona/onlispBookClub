e02069d2ce425d904b323d7e33be4fff1db31626
Ed MacDonald <edmacdonald555@gmail.com>
2021-10-09 13:11:40 -0400

    Can now run docker client (against hosts docker server) in the container!

--- environments/ed/env.sh
+++ environments/ed/env.sh
@@ -14,11 +14,13 @@ cd "${ENVIRONMENT_ROOT}/docker" && \
       --build-arg UID=$(id -u ${USER}) \
       --build-arg USERGROUP="${USER}" \
       --build-arg GID="$(id -g ${USER})" \
+      --build-arg DOCKER_GID="$(cat /etc/group | grep "^docker" | awk -F ":" '{print $3}')" \
       --build-arg GIT_USER_EMAIL="${GIT_USER_EMAIL}" \
       --build-arg GIT_USER_NAME="${GIT_USER_NAME}" \
       -t emacdona/onlisp .
 
 # https://jtreminio.com/blog/running-docker-containers-as-current-host-user/
+# https://jpetazzo.github.io/2015/09/03/do-not-use-docker-in-docker-for-ci/
 docker run \
    -v "${PROJECT_ROOT}":"/home/${USER}/onlisp" \
    -v "${ENVIRONMENT_ROOT}/docker/.exrc":"/home/${USER}/.exrc" \
@@ -26,6 +28,7 @@ docker run \
    -v "${ENVIRONMENT_ROOT}/docker/.zshrc":"/home/${USER}/.zshrc" \
    -v "${ENVIRONMENT_ROOT}/docker/.emacs":"/home/emacdona/.emacs" \
    -v "${HOME}/.ssh":"/home/${USER}/.ssh":ro \
+   -v /var/run/docker.sock:/var/run/docker.sock \
    -e "TERM=xterm-256color" \
    -it emacdona/onlisp $@
 
