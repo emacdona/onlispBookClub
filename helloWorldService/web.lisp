dc33171e1ed5cea56f3fa73022404d1dd2fbd6d8
Ed <edmacdonald555@gmail.com>
2022-01-23 20:15:11 +0000

    Parameterized creation of html endpoints, but it required an 'eval' :-/ Need to figure out a better way, but this will do for now

--- helloWorldService/web.lisp
+++ helloWorldService/web.lisp
@@ -2,6 +2,9 @@
 (defvar *acceptor* (make-instance 'hunchentoot:easy-acceptor :port 8282))
 (setf (hunchentoot:acceptor-document-root *acceptor*) #p".")
 
+;;
+;; "main" function used by buildapp
+;;
 (defun main (args)
   (declare (ignore args))
   (start-web)
@@ -22,22 +25,23 @@
            (uiop:quit)))
     (error (c) (format t "Woops, an unknown error occured:~&~a~&" c))))
 
+;;
+;; Starts web server. Called by "main", or directly from repl
+;;
 (defun start-web()
   (format *error-output* "Starting web server~%")
   (hunchentoot:start *acceptor*)
   (format *error-output*  "Web server started~%"))
 
+;;
+;; Stops web server
+;;
 (defun stop-web()
   (hunchentoot:stop *acceptor*))
 
-
-(defun hello()
-  (format nil "Hello, it works!"))
-
-(push
- (hunchentoot:create-prefix-dispatcher "/hello.html" #'hello)
- hunchentoot:*dispatch-table*)
-
+;;
+;; An endpoint that uses spinneret to generate a web page.
+;;
 (hunchentoot:define-easy-handler (say-hi :uri "/hi") ()
   (setf (hunchentoot:content-type*) "text/html")
   (spinneret:with-html-string
@@ -50,11 +54,33 @@
       (:p "Hello!"))))
   )
 
-(hunchentoot:define-easy-handler (say-yo :uri "/yo") (name)
-  (setf (hunchentoot:content-type*) "text/plain")
-  (format nil "Hey~@[ ~A~]!" name))
+;;
+;; What follows is an attempt at parameterizing the form above
+;;
 
-(hunchentoot:define-easy-handler (say-bye :uri "/bye") (name)
-  (setf (hunchentoot:content-type*) "text/plain")
-  (format nil "Bye~@[ ~A~]!" name))
+;; Wraps a "title" and "body" in boilerplate html
+(defmacro with-page ((&key title) &body body)
+  `(quote ((:doctype)
+     (:html
+      (:head
+       (:title ,title))
+      (:body ,@body)))))
+
+;; Just a thin wrapper around with-page, but shows how you can extend
+;; with-page to contain more structure ("boilerplate") in the body.
+(defmacro simple-page((&key title) &body body)
+  `(with-page (:title ,title) (:h1 ,title) ,@body))
+
+(defmacro endpoint((&key key) (&body body))
+  `(hunchentoot:define-easy-handler
+       (,(make-symbol (string-upcase (concatenate 'string "say-" key)))
+        :uri ,(concatenate 'string "/" key)) ()
+     (setf (hunchentoot:content-type*) "text/html")
+     (spinneret:with-html-string ,@body)))
+
+(defmacro html-endpoint((&key key) (&key title) &body body)
+  ; I don't know how to do this without an (eval)
+  `(endpoint (:key ,key) ,(eval `(simple-page (:title ,title) ,@body))))
+
+(html-endpoint (:key "hola") (:title "Hola!") (:p "Hola Mundo"))
 
