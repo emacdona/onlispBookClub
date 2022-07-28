61af183cd035bbf3d73154b74113b723a4468568
Ed <edmacdonald555@gmail.com>
2022-07-28 03:23:17 +0000

    adding doc strings; some refactoring of the class and method

--- projects/todo/units.lisp
+++ projects/todo/units.lisp
@@ -36,19 +36,24 @@
      (:centi . 1/100)
      (:milli . 1/1000))))
 
-(defmacro aget (key alist)
-  `(cdr (assoc ,key ,alist)))
+(defun aget (key alist)
+  "Get the value associated with key in assoc list."
+  (cdr (assoc key alist)))
 
 (defun unit-meta(unit)
+  "Get the metadata associated with unit in the unit database."
   (aget unit *unit-db*))
 
 (defun can-prefix(unit)
+  "Can the given unit be prefixed?"
   (aget :can-prefix (unit-meta unit)))
 
 (defun valid-unit-types(unit)
+  "Get the valid unit types for unit."
   (aget :type (unit-meta unit)))
 
 (defun default-unit-type(unit)
+  "Get the default unit type for unit -- IF it has one. nil otherwise."
   (let ((valid-types (valid-unit-types unit))
         (default-type (aget :default-type (unit-meta unit))))
     (cond (default-type default-type)
@@ -56,9 +61,11 @@
           (t nil))))
 
 (defun units()
+  "Get all unit names."
   (mapcar (lambda (x) (car x)) *unit-db*))
 
 (defun unit-types(&optional (name nil namep))
+  "Get the valid unit types for unit named by name -- if specified. Otherwise, get all unit types."
   (if namep
       (valid-unit-types name)
       (remove-duplicates
@@ -67,20 +74,12 @@
                       *unit-db*)))))
 
 (defclass unit ()
-  ((name
-    :initarg :name
-    :initform nil)
-   (type
-    :initarg :type
-    :initform nil)
-   (prefix
-    :initarg :prefix
-    :initform nil)))
+  ((name :initarg :name)
+   (type :initarg :type :initform nil)
+   (prefix :initarg :prefix)))
 
-(defmethod initialize-instance :after ((u unit) &key)
-  (let* ((name (slot-value u 'name))
-         (type (slot-value u 'type))
-         (default-type (default-unit-type name))
+(defmethod initialize-instance :before ((u unit) &key name type)
+  (let* ((default-type (default-unit-type name))
          (valid-types (unit-types name)))
     (cond
       ((not (member name (units)))
@@ -88,6 +87,9 @@
       ((and (not type) (not default-type))
        (error (format nil "Ambiguous unit. You MUST specify a type. One of: ~s" valid-types)))
       ((and type (not (member type valid-types)))
-       (error (format nil "The specified quantity type does not apply for ~s. Try one of ~s." name valid-types)))
-      ((not type)
-       (setf (slot-value u 'type) default-type)))))
+       (error (format nil "The specified quantity type does not apply for ~s. Try one of ~s." name valid-types))))))
+
+(defmethod initialize-instance :after ((u unit) &key)
+  (with-slots (name type) u
+    (if (not type)
+        (setf type (default-unit-type name)))))
